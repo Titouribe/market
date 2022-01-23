cbc755911d183b45bca666c34ea1717f0941ccd4
Jorge Uribe <juribe2897@gmail.com>
2022-01-23 15:36:22 -0500

    first step of the practice using spring boot marketAPI

---
+++ src/test/java/com/practice/market/PracticeMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
